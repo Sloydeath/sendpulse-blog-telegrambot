2f526b2f0b190ee6b3a0b59581249a4489bc12be
Sloydeath <panas.andrew@mail.ru>
2021-03-21 23:17:33 +0300

    SPTB-0: added skeleton of Spring Boot

---
+++ src/main/java/by/sendpulse/panas/sptb/SendPulseTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package by.sendpulse.panas.sptb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SendPulseTelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SendPulseTelegramBotApplication.class, args);
+	}
+
+}

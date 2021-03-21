8259a357651feb3b1244063104cc5cf3c90ddd3a
Sloydeath <panas.andrew@mail.ru>
2021-03-22 00:15:56 +0300

    SPTB-0: added skeleton of Spring Boot project

---
+++ src/main/java/by/github/sendpusle/sptb/SendPulseTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package by.github.sendpusle.sptb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SendPulseTelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SendPulseTelegramBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/github/sendpusle/sptb/SendPulseTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package by.github.sendpusle.sptb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SendPulseTelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
